a6c462bc8b9a1752582a06b740c91bc94112ba07
Tharusha Ariyarathna <tharushasandaru1111@gmail.com>
2026-07-02 15:26:44 +0530

    converting_user_input

---
+++ converting_user_input.py
@@ -0,0 +1,8 @@
+age = input('what is your age')
+print('age is =',age)
+print(type(age))
+
+
+
+value = input("Enter number: ") # User enters 5 -> value becomes "5"
+print(value * 3)                # Output: 555 (Repeats string 3 times instead of 5 * 3 = 15)
