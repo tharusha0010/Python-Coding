b1f773cef64e9f18a7e7cb026721e43fa7496e52
Tharusha Ariyarathna <tharushasandaru1111@gmail.com>
2026-07-04 21:22:41 +0530

    Assignment: Build Functions

--- Conditional_Statements.py
+++ Conditional_Statements.py
@@ -2,6 +2,6 @@ x = 5
 if x<10:
     print('x is smaller than 10')
     if x>20:
-        print('x is grater than 20')
+        print('x is greater than 20')
         
 print('Done with conditional statements')        
