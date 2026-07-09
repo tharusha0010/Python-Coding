418b4194f2cadc9f4feba97ff546e3d0831acdd5
Tharusha Ariyarathna <tharushasandaru1111@gmail.com>
2026-07-09 21:16:38 +0530

    vectorized math functions

---
+++ numpy/07.vectorized math functions.py
@@ -0,0 +1,18 @@
+import os
+import numpy as np
+os.system('cls')
+print("Vectorized math functions")
+
+arr = np.array([1,3,4,9,15])
+print(np.sqrt(arr))
+print(np.round(np.sqrt(arr))) 
+print(np.pi)
+
+#A = pi*r^2
+radius = np.array([1,2,3,4,5])
+print(np.pi*radius**2)
+
+
+
+
+
