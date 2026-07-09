218fe92aa2321e025c6335a3d68edaace872419b
Tharusha Ariyarathna <tharushasandaru1111@gmail.com>
2026-07-09 21:21:15 +0530

    Element wise arithmetic

---
+++ numpy/08.Element wise arithmetic.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+os.system('cls')
+print("Element-wise arithmetic operations")
+
+arr1 = np.array([1,2,3])
+arr2 = np.array([4,5,6])
+print(arr1 + arr2)
+print(arr1 - arr2)
+print(arr1 * arr2)
+print(arr1 / arr2)
+print(arr1 ** arr2)
