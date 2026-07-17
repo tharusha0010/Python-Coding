538d9a28cd0983ae290cb38d976596702a3c78d2
Tharusha Ariyarathna <tharushasandaru1111@gmail.com>
2026-07-17 18:57:41 +0530

    series

---
+++ Pandas/02.series.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+os.system('cls')
+
+print("integer")
+data = [100,102,104]
+series = pd.Series(data)
+print(series)
+
+"""
+print("Float")
+data1 = [100.1,102.2,103.3]
+series = pd.Series(data1)
+print(series)
+
+print("strings")
+data2 = ["A","B","C"]
+series = pd.Series(data2)
+print(series)
+
+"""
